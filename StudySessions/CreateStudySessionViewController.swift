748dc1125c14c5b631b3247a615c16be26f4508e
PrakharGarg <prakhargarg@utexas.edu>
2016-10-25 21:42:23 -0500

    create study session

--- StudySessions/CreateStudySessionViewController.swift
+++ StudySessions/CreateStudySessionViewController.swift
@@ -9,14 +9,60 @@
 import UIKit
 import Parse
 
-class CreateStudySessionViewController: UIViewController {
+class CreateStudySessionViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    @IBOutlet var name: UITextField!
+    
+    @IBOutlet var studySessionDescription: UITextField!
+    @IBOutlet var classPicker: UIPickerView!
 
-    override func viewDidLoad() {
+    @IBOutlet var course: UITextField!
+    @IBOutlet var time: UITextField!
+    @IBOutlet var location: UITextField!
+    let pickerView = UIPickerView()
+    
+    // Variable that holds all of the courses for the current user
+    var courses = [PFObject]()
+
+    
+    @IBAction func createSession(_ sender: AnyObject) {
+
+        let studySession = PFObject(className: "StudySessions")
+        studySession["name"] = name.text
+        studySession["description"] = studySessionDescription.text
+        studySession["location"] = location.text!
+        studySession["course"] = course.text
+        studySession["time"] = time.text
+        studySession["students"] = [(PFUser.current()?.objectId)!]
+        
+        studySession.saveInBackground { (success: Bool, error: Error?) in
+            if (success) {
+                print("YAY")
+                let alert = UIAlertController(title: "Success", message: String(describing: "You have created a Study Session"), preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                self.name.text = ""
+                self.studySessionDescription.text = ""
+                self.location.text = ""
+                self.course.text = ""
+                self.time.text = ""
+
+            } else {
+                // There was a problem, check error.description
+            }
+        }
+    }
+
+    
+   override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Create a Study Session"
-        
-
-        // Do any additional setup after loading the view.
+        hideKeyboardWhenTappedAround()
+        findCourses()
+    
+    
+        pickerView.delegate = self
+        course.inputView = pickerView
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,23 +70,56 @@ class CreateStudySessionViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // Find all of the courses for the current user by running a Parse query in the Courses model for the student's ID.
+    func findCourses() {
+        let user = PFUser.current()
+        let query = PFQuery(className:"Courses")
+        query.whereKey("students", equalTo:(user?.objectId)!)
+        query.findObjectsInBackground { (object: [PFObject]?, error: Error?) in
+            if error == nil {
+                self.courses = object!
+                self.pickerView.reloadAllComponents()
+            }
+        }
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return courses.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return courses[row]["name"] as! String?
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        course.text = courses[row]["name"] as! String?
+    }
+    
+    
+    
+    
+    
 // SAMPLE CREATE SESSION QUERY
-//    func createSesh(){
-//    var studySession = PFObject(className: "StudySessions")
-//    studySession["name"] = "Test #2"
-//    studySession["description"] = "foo"
-//    studySession["location"] = "PCL 3.12"
-//    studySession["course"] = "CS439"
-//    studySession["time"] = "5:30pm"
-//    studySession["students"] = ["SjestbPm0z"]
-//    
-//    studySession.saveInBackground { (success: Bool, error: Error?) in
-//        if (success) {
-//            print("YAY")
-//        } else {
-//        // There was a problem, check error.description
-//        }
-//    }
-//    }
+    func createSesh(){
+    var studySession = PFObject(className: "StudySessions")
+    studySession["name"] = "Test #2"
+    studySession["description"] = "foo"
+    studySession["location"] = "PCL 3.12"
+    studySession["course"] = "CS439"
+    studySession["time"] = "5:30pm"
+    studySession["students"] = ["SjestbPm0z"]
+    
+    studySession.saveInBackground { (success: Bool, error: Error?) in
+        if (success) {
+            print("YAY")
+        } else {
+        // There was a problem, check error.description
+        }
+    }
+    }
     
 }

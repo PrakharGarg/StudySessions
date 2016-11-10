d60c33a9ae2c2aa138ca2ddcf50a25979bc62298
PrakharGarg <prakhargarg@utexas.edu>
2016-11-10 17:32:22 -0600

    Nareen's code

--- StudySessions/StudySessionDetailViewController.swift
+++ StudySessions/StudySessionDetailViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class studySessionDetailViewController: UIViewController {
+class StudySessionDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
